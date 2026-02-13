587323d3f3a5b8b5e9adcdce9a0152ac6b07ac3c
euj1cob <sekar.kumaran@in.bosch.com>
2026-02-13 11:40:17 +0530

    Included cc

--- mail_generator.py
+++ mail_generator.py
@@ -1,6 +1,7 @@
 # !usr/bin/python3
 
 #03-02-2026 : Kumaran Sekar -   Intial Phase to Send the Mail with Attachment
+#13-02-2026 : Kumaran Sekar	-  	Included CC contacts in this script
 
 import win32com.client as win32
 from logging import exception
@@ -20,8 +21,9 @@ from os.path import exists
 
 class MailGenerator:
     
-    def __init__(self, To, Subject, Body, AttachmentFile):
+    def __init__(self, To, Cc, Subject, Body, AttachmentFile):
         self.To = To
+        self.Cc = Cc
         self.Subject = Subject
         self.Body = Body
         self.Attachment = AttachmentFile
@@ -31,6 +33,7 @@ class MailGenerator:
         outlook = win32.Dispatch('outlook.application')
         mail = outlook.CreateItem(0)
         mail.To = self.To
+        mail.Cc = self.Cc
         mail.Subject = str(self.Subject)
         mail.HtmlBody = str(self.Body)
         mail.Attachments.Add(self.Attachment)
@@ -40,12 +43,13 @@ if __name__ == "__main__":
     parser = optparse.OptionParser()
     
     parser.add_option('-t', '--to whom', dest='To', default =None, help='To who the Mail for the SAP Sheet')
+    parser.add_option('-c', '--Cc whom', dest='Cc', default =None, help='CC to whom the Mail for the SAP Sheet')
     parser.add_option('-s', '--Subject', dest='Subject', default =None, help='Subject of mail')
     parser.add_option('-b', '--Body', dest='Body', default =None, help='Body of the Mail for the SAP Sheet')
     parser.add_option('-a', '--attach', dest='Attachment', default =None, help='Attachment of the SAP Sheet')
     (options, args) = parser.parse_args()
     
-    Mg1 = MailGenerator(options.To, options.Subject, options.Body, options.Attachment)
+    Mg1 = MailGenerator(options.To, options.Cc,options.Subject, options.Body, options.Attachment)
     Mg1.maildrafter()
     endTime = datetime.now()
     print ("Execution Ends at ",endTime)
